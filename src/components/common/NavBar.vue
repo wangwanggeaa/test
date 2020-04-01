a61807a604066109185dbf1b09b740fed2a47eac
wangwanggeaa <1165254437@qq.com>
2020-04-01 16:28:11 +0800

    开发中

---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="navbar">
+      <div class="left" ><slot name="left"></slot></div>
+       <div class="center"><slot name="center"></slot></div>
+        <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navbar{
+    height: 44px;
+    text-align: center;
+    display: flex;
+    line-height: 40px;
+    box-shadow: 0 1px 1px rgba(100,100,100,.1);
+}
+.right{
+    width: 60px;
+    /* background-color: aqua; */
+}
+.center{
+    flex: 1;
+    /* background-color: black; */
+}
+.left{
+    width: 60px;
+    /* background-color: aqua; */
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,25 @@
 <template>
 <div>
+    <nav-bar class="home-nav">
+        <div  slot="center">购物街</div>
+    </nav-bar>
     <h1>首页</h1>
 </div>
 </template>
 
 <script>
+import NavBar from '../../components/common/NavBar'
 export default {
-
+components:{
+    NavBar
+}
 }
 </script>
 
 <style>
-
+@import '../../assets/css/base.css';
+.home-nav{
+    color: white;
+    background-color: var(--color-tint);
+}
 </style>
